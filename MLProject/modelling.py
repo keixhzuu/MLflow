9bc59e60c8bcf3f1b20ba246367e18815304b865
keixhzuu <melania.ints18@gmail.com>
2025-12-17 20:03:20 +0700

    Rombak dikit

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -11,21 +11,13 @@ df = pd.read_csv("dataset_preprocessing/Crop_Recommendation_preprocessing.csv")
 X = df.drop(columns=["Crop"])
 y = df["Crop"]
 
-# =========================
-# 2. Split data
-# =========================
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# =========================
-# 3. MLflow autolog
-# =========================
+
 mlflow.sklearn.autolog()
 
-# =========================
-# 4. Training model
-# =========================
 with mlflow.start_run():
     model = RandomForestClassifier(
         n_estimators=100,
@@ -34,9 +26,7 @@ with mlflow.start_run():
 
     model.fit(X_train, y_train)
 
-    # =========================
-    # 5. Evaluasi
-    # =========================
+
     y_pred = model.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
 
